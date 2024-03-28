28bb5ae56bc2c96fd6df97d53ed5ba68d5d1927a
danh mat em <145086417+dotruongan204@users.noreply.github.com>
2024-03-29 01:26:16 +0700

    FixCRUDInsuranceAndCRUDBeneficiary

--- HRManagement/src/java/controller/AllInsuranceController.java
+++ HRManagement/src/java/controller/AllInsuranceController.java
@@ -79,12 +79,9 @@ public class AllInsuranceController extends HttpServlet {
         ArrayList<InsuranceEmployeeDTO> typeList = insuranceDAO.getAllType();
         request.setAttribute("typeList", typeList);
         String typeName = request.getParameter("typeName");
-//        Date fromDate = null;
-//        Date toDate = null;
+        Date fromDate = null;
+        Date toDate = null;
         // Xử lý fromDate và toDate
-        Date now = new Date();
-        Date fromDate = now;
-        Date toDate = now;
         String fromDateStr = request.getParameter("fromDate");
         String toDateStr = request.getParameter("toDate");
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -120,6 +117,7 @@ public class AllInsuranceController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        headerInfor.setSessionAttributes(request);
     }
 
     /**
